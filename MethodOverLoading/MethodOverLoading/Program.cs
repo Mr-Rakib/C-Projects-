241d4293fbb95eb3ff02d5f28ca8060fa0d095be
Mr-Rakib <54507792+Mr-Rakib@users.noreply.github.com>
2019-12-03 15:23:30 +0600

    z

---
+++ MethodOverLoading/MethodOverLoading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MethodOverLoading
+{
+    class Program1
+    {
+        public virtual void add()
+        {
+            Console.WriteLine("This is add of Program without parameter");
+        }
+
+        public void add(int x)
+        {
+            Console.WriteLine(x+ " is generated the add with parameter");
+        }
+    }
+
+    class Program2 : Program1
+    {
+        public override void add()
+        {
+            Console.WriteLine("This is add with parameter from Program 2");
+        }
+    }
+
+
+
+    class Program : Program2
+    {
+        Program()
+        {
+            //Method_Overloading();
+            Method_Overriding();
+        }
+
+        private void Method_Overriding()
+        {
+            add();
+        }
+
+        private void Method_Overloading()
+        {
+            add();
+            add(5);
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadLine();
+        }
+    }
+}
