3a490c94a74e2ebd9f23b16e7b10e39ef33298c4
Mr-Rakib <54507792+Mr-Rakib@users.noreply.github.com>
2019-12-03 15:21:52 +0600

    final commit  of 3rd December

---
+++ Elements/Elements/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elements
+{
+    /// <summary>
+    /// All of the data-structures 
+    /// But all af those can store duplicate entry
+    /// </summary>
+    
+
+    class Program
+    {
+
+        Program()
+        {
+            Call_List();
+            Call_Stack(); 
+            Call_Queue();
+        }
+
+        private void Call_Queue()
+        {
+            Console.WriteLine("\nCall Queue of the Students :");
+            Queue<string> name = new Queue<string>();
+            name.Enqueue("Mostana Khan");
+            name.Enqueue("Nazia Srakar");
+            name.Enqueue("Marzia Hasan");
+
+            foreach (string n in name)
+            {
+                Console.WriteLine(n);
+            }
+
+            name.Dequeue();
+
+            Console.WriteLine();
+            Console.WriteLine("After Dequeue : ");
+            foreach (string n in name)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        private void Call_Stack()
+        {
+            Console.WriteLine("\nCall Stack of the Students :");
+            Stack <string> name = new Stack<string>();
+            name.Push("Mostana Khan");
+            name.Push("Nazia Srakar");
+            name.Push("Marzia Hasan");
+
+            foreach (string n in name)
+            {
+                Console.WriteLine(n);
+            }
+
+            name.Pop();
+
+            Console.WriteLine();
+            Console.WriteLine("After Poping : ");
+            foreach (string n in name)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        private void Call_List()
+        {
+            Console.WriteLine("Call List of the Students :");
+            List<string> name = new List<string>();
+            name.Add("Rakib Hasan");
+            name.Add("Hridoy Hasan");
+            name.Add("Monir Hasan");
+
+            foreach(string n in name)
+            {
+                Console.WriteLine(n);
+            }
+
+            name.Remove("Monir Hasan");
+
+            Console.WriteLine();
+            Console.WriteLine("After removing : ");
+            foreach (string n in name)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Some Collection/Some Collection/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Some_Collection
+{
+    class Program
+    { 
+        /// <summary>
+        /// HashSet is a updated version of list
+        /// SorrtedSet is the sorted version of HashSet
+        /// Where it not duplicate the items
+        /// 
+        /// LinkList support Duplicate entry 
+        /// But it can add and remove items in 
+        /// very fast time period
+        /// 
+        /// Sorted List, Sorted Dictionary & Dictionary are
+        /// in same data type 
+        /// holding the key according to value 
+        /// 
+        /// </summary>
+
+
+        Program()
+        {
+            Call_HashSet();
+            Call_SortedSet();
+            Call_LinkList();
+            Call_Dictionary();
+            Call_SortedDictionary();
+            Call_SortedList();
+
+        }
+
+        private void Call_SortedList()
+        {
+            Console.WriteLine("\nCall Sorted List : ");
+            SortedList<int, string> student = new SortedList<int, string>();
+            student.Add(1, "RAKIB");
+            student.Add(2, "HASAN");
+            student.Add(3, "MOTIN");
+
+            foreach (KeyValuePair<int, string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+            Console.Write("Enter the Index : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The name of this index is : " + student[i] + "\n");
+
+            student.Remove(i);
+            foreach (KeyValuePair<int, string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+        }
+
+        private void Call_SortedDictionary()
+        {
+            Console.WriteLine("\nCall Sorted  Dictionary : ");
+            SortedDictionary<int, string> student = new SortedDictionary<int, string>();
+            student.Add(1, "RAKIB");
+            student.Add(2, "HASAN");
+            student.Add(3, "MOTIN");
+
+            foreach (KeyValuePair<int, string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+            Console.Write("Enter the Index : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The name of this index is : " + student[i] + "\n");
+
+            student.Remove(i);
+            foreach (KeyValuePair<int, string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+        }
+
+        private void Call_Dictionary()
+        {
+            Console.WriteLine("\nCall Dictionary : ");
+            Dictionary<int, string> student = new Dictionary<int, string>();
+            student.Add(1,"RAKIB");
+            student.Add(2,"HASAN");
+            student.Add(3,"MOTIN");
+
+            foreach(KeyValuePair<int,string> kv in student)
+            {
+                Console.WriteLine(kv.Key+" "+kv.Value);
+            }
+
+            Console.Write("Enter the Index : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The name of this index is : " + student[i] +"\n");
+
+            student.Remove(i);
+            foreach (KeyValuePair<int, string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+        }
+
+        private void Call_LinkList()
+        {
+            Console.WriteLine("\nCall Linked List");
+            
+
+            LinkedList<string> student = new LinkedList<string>();
+            student.AddLast("RAKIB");
+            student.AddFirst("HRIDOY");
+            student.AddFirst("MONIR");
+            student.AddLast("RITA");
+
+            foreach(string n in student)
+            {
+                Console.WriteLine(n);
+            }
+
+            student.Remove("RAKIB");
+
+            Console.WriteLine("\nAfter removing");
+            foreach (string n in student)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            LinkedListNode<string> node = student.Find("HRIDOY");
+            student.AddAfter(node , "RAKIB");
+            student.AddBefore(node , "MINA");
+
+            Console.WriteLine("\nAfter Adding Middle");
+            foreach (string n in student)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+
+        private void Call_SortedSet()
+        {
+            Console.WriteLine("\nCall Sorted Set");
+            SortedSet<string> sorted = new SortedSet<string>();
+            sorted.Add("Md Rakibul Hasan");
+            sorted.Add("Md Nahidul Hasan");
+            sorted.Add("Md Rohan Hasan");
+
+            foreach (string h in sorted)
+            {
+                Console.WriteLine(h);
+            }
+
+            sorted.Remove("Md Rakibul Hasan");
+            Console.WriteLine("\nAfter removing ");
+            foreach (string h in sorted)
+            {
+                Console.WriteLine(h);
+            }
+        }
+
+        private void Call_HashSet()
+        {
+
+            Console.WriteLine("Call Hash Set");
+            HashSet<string> hash = new HashSet<string>();
+            hash.Add("Md Rakibul Hasan");
+            hash.Add("Md Nahidul Hasan");
+            hash.Add("Md Rohan Hasan");
+            
+            foreach(string h in hash)
+            {
+                Console.WriteLine(h);
+            }
+
+            hash.Remove("Md Rakibul Hasan");
+            Console.WriteLine("\nAfter removing ");
+            foreach (string h in hash)
+            {
+                Console.WriteLine(h);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadLine();
+        }
+    }
+}
