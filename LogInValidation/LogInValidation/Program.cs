7e4bf4e02f4b26ae22c247fd24eda042627a9aea
Mr-Rakib <54507792+Mr-Rakib@users.noreply.github.com>
2019-12-04 10:35:15 +0600

    validation

---
+++ LogInValidation/LogInValidation/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace LogInValidation
+{
+
+
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// Login Validaton
+        /// 
+        /// Conditions : (username)
+        /// 1) should not empty
+        /// 2) does not contain any special character
+        /// 3) should not a digit 
+        /// 
+        /// Conditions : (password)
+        /// 1) should not empty
+        /// 2) more than 6 character
+        /// 3) must contains atlest one Uppercase Character
+        /// 
+        /// </summary>
+
+
+        Program()
+        {
+                userLogin(); 
+        }
+
+        
+
+        private void userLogin()
+        {
+            l1:
+            Console.Write("Enter Username : ");
+            string username = Console.ReadLine();
+        
+            if ( isValidUserName(username) )
+            {
+                l2:
+                Console.Write("Enter Password : ");
+                string password = Console.ReadLine();
+               
+                    if (isValidPassword(password))
+                    {
+                        Console.WriteLine("Successfully Loged in"); 
+                    }else
+                    {
+                        Console.WriteLine("invalid Password");
+                        goto l2;
+                    }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Username");
+                goto l1; 
+            }
+        }
+
+        private Boolean isValidPassword(string password)
+        {
+            if (! password.Equals("") && password.Length >= 6 && password.Any(char.IsUpper))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private Boolean isValidUserName(string username)
+        {
+           if( username.Equals("") || !username.All(c => char.IsLetterOrDigit(c)) || username.All(char.IsDigit) ) 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadLine();
+        }
+    }
+}
